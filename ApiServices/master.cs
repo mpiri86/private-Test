2823479531c75a841bb50f46d2a46b060fa456d7
mpiri86 <68366316+mpiri86@users.noreply.github.com>
2023-01-18 09:30:52 -0800

    Create master.cs
    create master.cs

---
+++ ApiServices/master.cs
@@ -0,0 +1,66 @@
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+
+namespace Api_Integrated_Test
+{
+   
+    public class master
+    {
+        
+
+        public master()
+        {
+        }
+     
+        public string WriteResultTextBox(string newStr)
+        {
+            return newStr + System.Environment.NewLine;
+        }
+        public async Task<string> Response_writeTofileAsync(HttpResponseMessage Response)
+        {
+
+            string generateStr;
+            //Writing text to the file.
+            generateStr =DateTime.Now.ToString() + System.Environment.NewLine;
+
+            generateStr += "StatusCode :" + Response.StatusCode.ToString() + System.Environment.NewLine;
+           // generateStr += "STatusCode :" + Response.StatusCode.ToString() + System.Environment.NewLine;
+
+            if(Response.StatusCode != System.Net.HttpStatusCode.RequestTimeout)
+            {
+                
+                try
+                {
+                    string ContentStr = await Response.Content.ReadAsStringAsync();
+                    ContentStr = ContentStr.Replace("{", "{"+ System.Environment.NewLine);
+                    ContentStr = ContentStr.Replace("}", System.Environment.NewLine + "}");
+                   // ContentStr = ContentStr.Replace(",", "," + System.Environment.NewLine);
+                    ContentStr = ContentStr.Replace("[", "[" + System.Environment.NewLine);
+                    ContentStr = ContentStr.Replace("]",  System.Environment.NewLine + "]");
+
+
+
+                    generateStr += "//////////Body :" + System.Environment.NewLine+ System.Environment.NewLine;
+                    generateStr += ContentStr + System.Environment.NewLine;
+                }
+                catch (Exception e)
+                {
+                    generateStr += "Error   :  Cant convert Body To String ..." + System.Environment.NewLine;
+
+                }
+            }
+            
+
+            return generateStr;
+
+
+        }
+
+        
+    }
+}
