b9bda6e6c8802b7bd4d212029525df4ac6704f1c
Masoud <piryaee@gmail.com>
2023-01-18 09:23:53 -0800

    comit stage form1 change1

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//change 1
 namespace CentralInsuranceCargoPolicyTest
 {
     public partial class CargoTestForm : Form
